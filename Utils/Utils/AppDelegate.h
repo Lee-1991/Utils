9ac6d2c5bb7e884b9af360f40958eeb15947da7f
lee <lee@wanba123.cn>
2017-06-14 14:38:15 +0800

    创建初始工程

---
+++ Utils/Utils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Utils
+//
+//  Created by lee on 2017/6/14.
+//  Copyright © 2017年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Utils/Utils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Utils
+//
+//  Created by lee on 2017/6/14.
+//  Copyright © 2017年 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
